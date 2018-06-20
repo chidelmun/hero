a25bf8a0379dcb18c7f20a9882ecfd1dc83cd26b
Delveri Chick <chickdelveri@gmail.com>
2018-06-20 17:49:20 -0400

    Added new UI layout

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Welcome to My App. I am running on Heroku..
-        </p>
+      <div className="container">
+          <div className="jumbotron">
+
+          </div>
+          <hr/>
+          <div className="row">
+            <div className="col-md-6 left"> </div>
+            <div className="col-md-6 right"> </div>
+          </div>
+
+          <hr/>
       </div>
     );
   }
