2ae702ebf4aee0ff64f653b47937f9f0bd2172a6
Delveri  M. Chick <chickdelveri@gmail.com>
2019-04-10 05:57:29 -0400

    WAK-1 Add entry point in index file

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+// This is the entry point of the app
